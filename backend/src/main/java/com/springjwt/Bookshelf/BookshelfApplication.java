2df83b1e168a4cc67d3a221336dceba6b8bb2dd4
Ela <103253928+ela-codes@users.noreply.github.com>
2024-08-13 14:03:30 -0500

    Add back & front end folder structures

---
+++ backend/src/main/java/com/springjwt/Bookshelf/BookshelfApplication.java
@@ -0,0 +1,13 @@
+package com.springjwt.Bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshelfApplication.class, args);
+	}
+
+}
